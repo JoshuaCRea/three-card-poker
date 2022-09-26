f80c4f609c57c3c44b3adfd996e859fe96a64805
Joshua Rea <josh.rea@gmail.com>
2022-09-26 12:19:34 -0400

    Created deck, shuffler, dealing to hand for player... assigned functionality to 'deal' button, prevented further wagers once hand is dealt.

--- three-card-poker.js
+++ three-card-poker.js
@@ -6,6 +6,8 @@ let anteWager = 0;
 let playWager = 0;
 let pairPlusWager = 0;
 let sixCardBonusWager = 0;
+let isRoundActive = false;
+let deck = [];
 
 function _getSelectedWagerAmount() {
     const radioButtons = document.getElementsByName("wager-amount");
@@ -44,16 +46,57 @@ function placeWager(wagerAmount, wagerType) {
     }
 }
 
+function dealToPlayer() {
+    if (anteWager === 0 || isRoundActive) {
+        return;
+    }
+    isRoundActive = true;
+    deck = getShuffledDeck();
+    const playerHand = deck.slice(0, 3);
+    displayHand(playerHand, "player");
+}
+
+function displayHand(hand, person) {
+    const handDisplay = hand.map(card => `<img class='card' src='cards/${card}.svg'></img>`);
+    $(`#${person}-card-display`).html(handDisplay);
+}
+
+function getShuffledDeck() {
+    const newDeck = [
+        '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH',
+        '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS',
+        '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD'
+    ];
+    for (let i = 0; i < newDeck.length; i++) {
+        const tempCard = newDeck[i];
+        const randomIndex = Math.floor(Math.random() * newDeck.length);
+        newDeck[i] = newDeck[randomIndex];
+        newDeck[randomIndex] = tempCard;
+    }
+    return newDeck;
+}
+
 window.onload = () => {
     $("#ante-diamond").click(function () {
+        if (isRoundActive) {
+            return;
+        }
         placeWager(_getSelectedWagerAmount(), "ANTE");
     });
     $("#pair-plus-wager-circle").click(function () {
+        if (isRoundActive) {
+            return;
+        }
         placeWager(_getSelectedWagerAmount(), "PAIR_PLUS");
     });
     $("#six-card-wager-circle").click(function () {
+        if (isRoundActive) {
+            return;
+        }
         placeWager(_getSelectedWagerAmount(), "SIX_CARD_BONUS");
     });
     $("#player-balance-display").html(`$${playerBalance}`);
     $("#total-wager-display").html(`$${totalWagerAmount}`);
+    $("#deal-button").click(() => dealToPlayer());
 }
